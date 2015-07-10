f746aa47666afae29c9119482c5c3b27cf74339c
AgileLabs <samer@agilelabs.com>
2015-07-10 13:07:40 -0700

    Passport

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var session = require('express-session');
+var configDB = require('./config/database.js');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+mongoose.connect(configDB.url);
 
 var app = express();
 
@@ -20,15 +23,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'thisisabadsecret', resave: true, saveUninitialized: true }));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+require('./config/passport')(passport);
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use('/', routes);
-app.use('/users', users);
+var router = require('./routes/index')(passport);
+app.use('/', router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,12 @@
-// do the magic
+var gulp = require("gulp");
+var concat = require("gulp-concat");
+
+gulp.task("default", function() {
+  gulp.watch("./source/javascripts/**/*.js", ["js"]);
+});
+
+gulp.task("js", function() {
+  gulp.src("./source/javascripts/**/*.js")
+  .pipe(concat("bundle.js"))
+  .pipe(gulp.dest("./public/javascripts/"))
+});
